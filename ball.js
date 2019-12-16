dfedce2ff74d745e0053f572d6ec91377876f5aa
Lene Katharina Pedersen <lkpedersen@lyse.net>
2019-12-16 09:31:18 +0100

    First upload

---
+++ ball.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var raf;
+
+var ball = {
+  x: 100,
+  y: 100,
+  vx: 5,
+  vy: 2,
+  radius: 25,
+  color: 'white',
+  opacity: 0.5,
+  draw: function() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    //ctx.fillStyle = this.color;
+    ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+    ctx.fill();
+  }
+};
+
+function draw() {
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+  ball.draw();
+  ball.x += ball.vx;
+  ball.y += ball.vy;
+  raf = window.requestAnimationFrame(draw);
+  
+  if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+    ball.vy = -ball.vy;
+  }
+  if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+    ball.vx = -ball.vx;
+  }
+}
+
+
+window.requestAnimationFrame(draw);
+
+
+ball.draw();
